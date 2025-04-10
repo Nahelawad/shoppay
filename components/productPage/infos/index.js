6b8689ffa84d2959f90b3716693c81029216eff1
nahel <nahelawad123@gmail.com>
2025-04-11 00:37:48 +0100

    css adjusments

--- components/productPage/infos/index.js
+++ components/productPage/infos/index.js
@@ -221,7 +221,6 @@ export default function Infos({product, setActiveImg}){
                  <Share/>
                  <Accordian details={[product.description,...product.details]}/>
                  
-                 
 
 
 
