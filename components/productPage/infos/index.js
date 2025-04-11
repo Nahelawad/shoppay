97acf77de7dc9da1080d82f1528c6a9cd30f9452
nahel <nahelawad123@gmail.com>
2025-04-11 03:23:17 +0100

    removed similar

--- components/productPage/infos/index.js
+++ components/productPage/infos/index.js
@@ -221,6 +221,7 @@ export default function Infos({product, setActiveImg}){
                  <Share/>
                  <Accordian details={[product.description,...product.details]}/>
                  
+                 
 
 
 
