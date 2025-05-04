359a440a79bf6ed81931c5eb85c4526dcc1a268c
nahel <nahelawad123@gmail.com>
2025-05-04 05:37:22 +0100

    wish list

--- pages/profile/wishlist.js
+++ pages/profile/wishlist.js
@@ -1,28 +1,54 @@
 import Layout from "../../components/profile/layout";
 import { getSession } from "next-auth/react";
-import User from "../../models/User"
+import User from "../../models/User";
+import styles from "../../styles/profile.module.scss";
 
-
-export default function Addresses({user,tab}){
+export default function Wishlist({ user, tab, wishlist }) {
     return (
         <Layout session={user.user} tab={tab}>
-            WELCOME TO YOUR WISHLIST PAGE
+            <div className={styles.orders}>
+                <div className={styles.header}>
+                    <h2>YOUR WISHLIST</h2>
+                </div>
+                {wishlist.length === 0 ? (
+                    <p>No items in wishlist.</p>
+                ) : (
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Product</td>
+                                <td>Category</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {wishlist.map((item) => (
+                                <tr key={item.product._id}>
+                                    <td>{item.product.name}</td>
+                                    <td>{item.product.category}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
+            </div>
         </Layout>
-    )
+    );
 }
 
+export async function getServerSideProps(ctx) {
+    const { query, req } = ctx;
+    const session = await getSession({ req });
+    const tab = query.tab || 0;
 
+    const wishlistData = await User.findById(session.user.id)
+        .select("wishlist")
+        .populate("wishlist.product")
+        .lean();
 
-export async function getServerSideProps(ctx){
-    const {query,req}=ctx;
-    const session= await getSession({req});
-    const tab=query.tab || 0;
-
-    const wishlistData=await User.findById(session.user.id).select("wishlist").populate("wishlist.product").lean();
-    return{
-        props:{
-           user: session,
-           wishlist:JSON.parse(JSON.stringify(wishlistData.wishlist)),
+    return {
+        props: {
+            user: session,
+            wishlist: JSON.parse(JSON.stringify(wishlistData.wishlist)),
             tab,
         },
     };
